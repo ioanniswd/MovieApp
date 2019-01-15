702b7280097a2ae686da45260fc64767a07bed47
ioanniswd <ioanniswd@gmail.com>
2019-01-15 15:06:48 +0200

    Added movie index and show.
     On branch master
    
     Changes to be committed:
    	new file:   app/assets/javascripts/movies.coffee
    	modified:   app/assets/stylesheets/application.css
    	new file:   app/assets/stylesheets/movies.scss
    	new file:   app/controllers/movies_controller.rb
    	new file:   app/helpers/movies_helper.rb
    	modified:   app/views/layouts/application.html.erb
    	new file:   app/views/movies/index.html.erb
    	new file:   app/views/movies/show.html.erb
    	modified:   config/routes.rb
    	new file:   test/controllers/movies_controller_test.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get '/movies' => 'movies#index'
+  get '/movies/:id' => 'movies#show', as: :movie
+  root 'movies#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
